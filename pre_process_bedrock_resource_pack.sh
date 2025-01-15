5127a289640887aba9996348f37cfd999ad77e26
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-01-14 21:55:44 -0300

    Add the old item models for java2bedrock.sh
    Maybe in the future we'll need to create a proper resource pack compiler for these resource pack variations

--- pre_process_bedrock_resource_pack.sh
+++ pre_process_bedrock_resource_pack.sh
@@ -1,2 +1,5 @@
 # Filled Maps, because items that replace filled maps do not work correctly in Bedrock (you can't see the map image)
 rm assets/minecraft/items/filled_map.json
+
+# Copy the overlay bedrock folder to here folder
+cp -r overlay_bedrock/ .
